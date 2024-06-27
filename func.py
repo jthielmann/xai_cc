876ab915664422d9abb30285c5e5620d58b803a9
JonathanThielmann <jonathi@outlook.de>
2024-06-27 19:12:54 +0200

    data preparation till merge.csv

--- func.py
+++ func.py
@@ -210,8 +210,81 @@ def load_dataset(data_dir, gene, columns_of_interest):
         train_st_dataset = pd.concat([train_st_dataset, st_dataset_filtered])  # concat all samples
 
 
+class STDataset(torch.utils.data.Dataset):
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        gene_names = list(self.dataframe)[1:]
+        gene_vals = []
+        row = self.dataframe.iloc[index]
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.7406, 0.5331, 0.7059], [0.1651, 0.2174, 0.1574])
+            # apply normalization transforms as for pretrained colon classifier
+        ])
+        a = Image.open(row["tile"]).convert("RGB")
+        # print(x.size)
+        for j in gene_names:
+            gene_val = float(row[j])
+            gene_vals.append(gene_val)
+        e = row["tile"]
+        a = transform(a)
+        return a, gene_vals, e
+
+
+class Subset(torch.utils.data.Dataset):
+    def __init__(self, subset, transform=None):
+        self.subset = subset
+        self.transform = transform
+
+    # get image and label tensor from dataset and transform
+    def __getitem__(self, index):
+        a, gene_vals, e = self.subset[index]
+        if self.transform:
+            a = self.transform(a)
+        return (a, gene_vals, e)
+
+    # get length of dataset
+    def __len__(self):
+        return len(self.subset)
+
+
+def get_patient_loader(data_dir, batch_size, patient=None, gene="RUBCNL"):
+    columns_of_interest = ["tile", gene]
+
+    train_st_dataset = pd.DataFrame(columns=columns_of_interest)
+
+    # generate training dataframe with all training samples
+    st_dataset = pd.read_csv(data_dir + patient + "/Preprocessed_STDataset/gene_data.csv", index_col=-1)
+    st_dataset["tile"] = st_dataset.index
+    st_dataset['tile'] = st_dataset['tile'].apply(lambda x: str(data_dir) + "/" + str(patient) + "/Tiles_156/" + str(x))
+
+    train_st_dataset = pd.concat([train_st_dataset, st_dataset[columns_of_interest]])  # concat all samples
+
+    train_st_dataset.reset_index(drop=True, inplace=True)
+    loaded_train_dataset = STDataset(train_st_dataset)
+    """
+    # Training and validation transforms (i.e. train data augmentation)
+    train_transforms = transforms.RandomApply([transforms.RandomRotation(degrees=
+                                                                         (0, 180)),
+                                               transforms.RandomHorizontalFlip(
+                                                   p=0.75),
+                                               transforms.RandomVerticalFlip(
+                                                   p=0.75)], p=0.5)
+
+    train_data = Subset(loaded_train_dataset, transform=train_transforms)
+    """
+    #val_data = Subset(loaded_valid_dataset, transform=train_transforms)
+    #train_loader = DataLoader(dataset=loaded_train_dataset, batch_size=batch_size, shuffle=False)
+    return loaded_train_dataset
+
+
 def get_data_loaders(data_dir, batch_size, gene="RUBCNL"):
-    print(getframeinfo(currentframe()).lineno)
     train_samples = ["p007", "p014", "p016", "p020", "p025"]
     val_samples = ["p009", "p013"]  # 8 21 26 page 42
 
@@ -243,54 +316,10 @@ def get_data_loaders(data_dir, batch_size, gene="RUBCNL"):
     train_st_dataset.reset_index(drop=True, inplace=True)
     valid_st_dataset.reset_index(drop=True, inplace=True)
 
-    class STDataset(torch.utils.data.Dataset):
-        def __init__(self, dataframe):
-            self.dataframe = dataframe
 
-        def __len__(self):
-            return len(self.dataframe)
-
-        def __getitem__(self, index):
-            gene_names = list(self.dataframe)[1:]
-            gene_vals = []
-            row = self.dataframe.iloc[index]
-            transform = transforms.Compose([
-                transforms.Resize((224, 224)),
-                transforms.ToTensor(),
-                transforms.Normalize([0.7406, 0.5331, 0.7059], [0.1651, 0.2174, 0.1574])
-                # apply normalization transforms as for pretrained colon classifier
-            ])
-            a = Image.open(row["tile"]).convert("RGB")
-            # print(x.size)
-            for j in gene_names:
-                gene_val = float(row[j])
-                gene_vals.append(gene_val)
-            e = row["tile"]
-            a = transform(a)
-            return (a, gene_vals, e)
 
     loaded_train_dataset = STDataset(train_st_dataset)
     loaded_valid_dataset = STDataset(valid_st_dataset)
-
-    # ------------------------------------------------------------------------------
-
-    # Apply training and validation transforms
-    # ------------------------------------------------------------------------------
-    class Subset(torch.utils.data.Dataset):
-        def __init__(self, subset, transform=None):
-            self.subset = subset
-            self.transform = transform
-
-        # get image and label tensor from dataset and transform
-        def __getitem__(self, index):
-            a, gene_vals, e = self.subset[index]
-            if self.transform:
-                a = self.transform(a)
-            return (a, gene_vals, e)
-
-        # get length of dataset
-        def __len__(self):
-            return len(self.subset)
 
     # Training and validation transforms (i.e. train data augmentation)
     train_transforms = transforms.RandomApply([transforms.RandomRotation(degrees=
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from func import get_model, training
 
 # for logging purposes
 training(resnet=get_model(),
-         data_dir='../Training_Data/',
+         data_dir='./data/',
          epochs=30,
          loss_fn=nn.MSELoss(),
          learning_mode="LLR",
