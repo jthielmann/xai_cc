ddb77bf49d7b37c3a55e5550181a60d60be9985c
JonathanThielmann <jonathi@outlook.de>
2024-07-19 14:13:26 +0200

    restructuring

--- script/newStructure/plot_and_print.py
+++ script/newStructure/plot_and_print.py
@@ -43,33 +43,3 @@ def print_metrics(data_dir, patient, metric):
     if metric == "std":
         print("std diff: ", "{:.4f}".format(abs(std_out - std_labels)), ", std out: ", "{:.4f}".format(std_out),
               ", std labels: ", "{:.4f}".format(std_labels))
-
-
-def plot_hist_comparison(data_dir, patient):
-    base_path = data_dir + patient + "/Preprocessed_STDataset/"
-    merge = pd.read_csv(base_path + "merge.csv")
-    out = merge['output']
-    plt.hist(out)
-    plt.show()
-    labels = merge['labels']
-    plt.hist(labels)
-    plt.show()
-
-
-def print_metrics(data_dir, patient, metric):
-    base_path = data_dir + patient + "/Preprocessed_STDataset/"
-    merge = pd.read_csv(base_path + "merge.csv")
-    out = merge['output']
-    labels = merge['labels']
-    mean_out = out.mean()
-    std_out = out.std()
-    mean_labels = labels.mean()
-    std_labels = labels.std()
-    if metric == "mean":
-        print("mean diff: ", "{:.4f}".format(abs(mean_out - mean_labels)), ", mean out: ", "{:.4f}".format(mean_out),
-              ", mean labels: ", "{:.4f}".format(mean_labels))
-    if metric == "std":
-        print("std diff: ", "{:.4f}".format(abs(std_out - std_labels)), ", std out: ", "{:.4f}".format(std_out),
-              ", std labels: ", "{:.4f}".format(std_labels))
-    if metric == "pearson":
-        print("pearson corr: " + str(round(scipy.stats.pearsonr(out, labels)[0], 2)))
