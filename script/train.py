9ed462ebb05d80186c78b1e9344a0c061b4f4752
JonathanThielmann <jonathi@outlook.de>
2024-10-24 01:29:56 +0200

    added model save for ae resnet18

--- script/train.py
+++ script/train.py
@@ -273,7 +273,10 @@ def train_decoder(model, criterion, optimizer, device, genes=None):
                 print(f'[Epoch {epoch + 1}, Batch {i + 1}] loss: {running_loss / 100:.4f}')
                 running_loss = 0.0
 
+        model.save("../models/ae_double_resnet18/ep_" + str(epoch) + ".pt")
+
     print('Finished Training')
+
 
 
 # Initialize the decoder, loss function, and optimizer
