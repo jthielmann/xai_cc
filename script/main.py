9f43b364e9c8865e326b7767c2b18cbacdfb309d
JonathanThielmann <jonathi@outlook.de>
2025-12-11 00:30:21 +0100

    added sweep search roots

--- script/main.py
+++ script/main.py
@@ -209,7 +209,7 @@ def _resolve_config_path(name: str) -> str:
     if os.path.isfile(name):
         return name
     # search common config roots
-    roots = ["../sweeps/missing", "../sweeps/configs", "../sweeps/to_train"]
+    roots = ["../sweeps/missing", "../sweeps/configs", "../sweeps/to_train", "best_genes", "sae"]
     for root in roots:
         cand = os.path.join(root, name)
         if os.path.isfile(cand):
